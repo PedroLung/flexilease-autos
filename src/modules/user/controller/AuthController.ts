9f3308af435996f31c0ea0630dbd43acb554d69b
PedroLung <pguedeslsilva@gmail.com>
2024-07-05 15:50:37 -0300

    feat: create auth controller

---
+++ src/modules/user/controller/AuthController.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import schemaAuthCreate from "../validations/authCreateValidation";
+import AuthService from "../services/AuthService";
+
+export default class AuthController {
+  authService = new AuthService();
+
+  createAuth = async (req: Request, res: Response) => {
+    try {
+      const { error, value } = schemaAuthCreate.validate(req.body);
+
+      if (error) {
+        const err = {
+          message: error.details[0].message,
+          field: error.details[0].context?.label,
+          status: "Failed",
+        };
+        return res.status(400).json(err);
+      }
+
+      const initAuth = await this.authService.createAuth(value);
+
+      res.status(201).json({ data: initAuth });
+    } catch (error) {
+      return res.status(400).json({
+        message: error,
+        status: "Bad Request",
+      });
+    }
+  };
+}
