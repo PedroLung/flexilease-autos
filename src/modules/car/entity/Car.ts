2c80fb7b796ca21bf900a9fada21e9bbad3f12c7
PedroLung <pguedeslsilva@gmail.com>
2024-07-05 11:39:42 -0300

    fix: several fixes and adjusts

--- src/modules/car/entity/Car.ts
+++ src/modules/car/entity/Car.ts
@@ -26,8 +26,8 @@ export default class Car {
   @Column()
   value_per_day: number;
 
-  @Column("simple-array")
-  acessories: string[];
+  @Column()
+  accessories: Accessory[];
 
   @Column()
   number_of_passengers: number;
@@ -40,28 +40,9 @@ export default class Car {
 
   @DeleteDateColumn()
   deleted_at: Timestamp;
+}
 
-  constructor(
-    _id: ObjectId,
-    model: string,
-    color: string,
-    year: string,
-    value_per_day: number,
-    acessories: string[],
-    number_of_passengers: number,
-    created_at: Timestamp,
-    updated_at: Timestamp,
-    deleted_at: Timestamp,
-  ) {
-    (this._id = _id),
-      (this.model = model),
-      (this.color = color),
-      (this.year = year),
-      (this.value_per_day = value_per_day),
-      (this.acessories = acessories),
-      (this.number_of_passengers = number_of_passengers),
-      (this.created_at = created_at),
-      (this.updated_at = updated_at),
-      (this.deleted_at = deleted_at);
-  }
+interface Accessory {
+  itemId?: number; // itemId is optional as it will be auto-generated
+  description: string;
 }
