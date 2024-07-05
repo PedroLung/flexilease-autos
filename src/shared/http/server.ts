6228f8e36f8bd2f9062d36f58f91bd38681d676b
PedroLung <pguedeslsilva@gmail.com>
2024-07-05 11:51:07 -0300

    feat: add routes in server.ts

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,11 +1,14 @@
 import { AppDataSource } from "../typeorm/data-source";
 import express from "express";
+import routes from "./routes";
 
 const app = express();
 
 const port = 3000;
 
 app.use(express.json());
+
+app.use("/api/v1", routes);
 
 AppDataSource.initialize()
   .then()
