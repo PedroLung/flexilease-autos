9999af1de77f7d39cd8ed3ce86b54f8b3dde5d30
PedroLung <pguedeslsilva@gmail.com>
2024-07-05 11:56:00 -0300

    feat: add routes

---
+++ src/shared/http/routes/index.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import carRoutes from "../../../modules/car/routes/car.routes";
+import userRoutes from "../../../modules/user/routes/user.routes";
+import authRoutes from "../../../modules/user/routes/auth.routes";
+import authMiddleware from "../../../middlewares/auth";
+
+const routes = Router({
+  mergeParams: true,
+});
+
+routes.use("/user", userRoutes);
+routes.use("/authenticate", authRoutes);
+
+routes.use(authMiddleware);
+
+routes.use("/car", carRoutes);
+
+export default routes;
