e6c36dae456b421f30fd24083832280be5856b2a
PedroLung <pguedeslsilva@gmail.com>
2024-07-07 15:16:24 -0300

    docs: create swagger

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,10 +1,14 @@
 import { AppDataSource } from "../typeorm/data-source";
 import express from "express";
 import routes from "./routes";
+import swaggerUi from "swagger-ui-express";
+import swaggerDoc from "../../../swagger.json";
 
 const app = express();
 
 const port = 3000;
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.use(express.json());
 
