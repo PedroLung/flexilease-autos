322dba3b381b01b409a35b19c6c12c31e0cf2ba0
PedroLung <pguedeslsilva@gmail.com>
2024-07-07 10:47:05 -0300

    feat: create reserve routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import carRoutes from "../../../modules/car/routes/car.routes";
 import userRoutes from "../../../modules/user/routes/user.routes";
+import reserveRoutes from "../../../modules/reserves/routes/reserve.routes";
 import authRoutes from "../../../modules/user/routes/auth.routes";
 import authMiddleware from "../../../middlewares/auth";
 
@@ -14,5 +15,6 @@ routes.use("/authenticate", authRoutes);
 routes.use(authMiddleware);
 
 routes.use("/car", carRoutes);
+routes.use("/reserve", reserveRoutes);
 
 export default routes;
